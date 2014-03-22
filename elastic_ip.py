b33abe2cf77697526fa206a280b640c5d5fe5507
John C. Earls <john.c.earls@gmail.com>
2014-03-22 22:51:31 +0000

    looks for unallocated ips

--- elastic_ip.py
+++ elastic_ip.py
@@ -5,5 +5,14 @@ import boto.utils
 instance = boto.utils.get_instance_identity()['document']['instanceId']
 
 ec2 = boto.connect_ec2()
-eip = boto.ec2.address.Address( ec2, public_ip='184.73.161.43')
-eip.associate(instance)
+success = False
+for address in ec.get_all_addresses():
+    try:
+        eip = boto.ec2.address.Address( ec2, public_ip=address)
+        res = eip.associate(instance)
+        if res:
+            print "Associated with %s" % address
+            break
+    except:
+        print "Unable to associate with %s" % address 
+
