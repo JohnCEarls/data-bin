0225594681deb4bc399e5495067814d77c727156
John C. Earls <yt.rabb@gmail.com>
2014-05-17 15:56:25 -0700

    site is determined by branch

--- elastic_ip.py
+++ elastic_ip.py
@@ -3,8 +3,12 @@ import boto
 import boto.utils
 import sys
 
-site_name = sys.argv[1]
+site_name = 'aurea-nebula-dev.adversary.us'
+instance = boto.utils.get_instance_identity()['document']['instanceId']
 def get_ip( site_name ):
+    """
+    Get the ip for this site from route 53 based on site name
+    """
     r53 = boto.connect_route53()
     zone = r53.get_zone('.'.join(site_name.split('.')[1:]) + '.')
     record = zone.get_a(site_name).resource_records
@@ -16,16 +20,17 @@ def get_ip( site_name ):
     return record 
 
 
-instance = boto.utils.get_instance_identity()['document']['instanceId']
-ec2 = boto.connect_ec2()
-success = False
-for address in ec2.get_all_addresses(get_ip(site_name)):
-    try:
-        res = address.associate(instance)
-        if res:
-            print "Associated with %s" % address
-            break
-    except Exception as e:
-        print e
-        print "Unable to associate with %s" % address 
+def associate_ip( instance_id, site_name ):
+    ec2 = boto.connect_ec2()
+    success = False
+    for address in ec2.get_all_addresses(get_ip(site_name)):
+        try:
+            res = address.associate(instance)
+            if res:
+                print "Associated with %s" % address
+                break
+        except Exception as e:
+            print e
+            print "Unable to associate with %s" % address 
 
+associate_ip( instance, site_name)
